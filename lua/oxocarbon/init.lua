dd9cb157776e8e7aa14ae6d5954868474cd24e70
Zafar Khodjaev <thezaff@users.noreply.github.com>
2024-07-11 15:10:59 +0500

    Update init.lua (change terminal colors)

--- lua/oxocarbon/init.lua
+++ lua/oxocarbon/init.lua
@@ -10,22 +10,22 @@ local base00 = "#161616"
 local base06 = "#ffffff"
 local base09 = "#78a9ff"
 local oxocarbon = (((vim.o.background == "dark") and {base00 = base00, base01 = blend_hex(base00, base06, 0.085), base02 = blend_hex(base00, base06, 0.18), base03 = blend_hex(base00, base06, 0.3), base04 = blend_hex(base00, base06, 0.82), base05 = blend_hex(base00, base06, 0.95), base06 = base06, base07 = "#08bdba", base08 = "#3ddbd9", base09 = base09, base10 = "#ee5396", base11 = "#33b1ff", base12 = "#ff7eb6", base13 = "#42be65", base14 = "#be95ff", base15 = "#82cfff", blend = "#131313", none = "NONE"}) or {base00 = base06, base01 = blend_hex(base00, base06, 0.95), base02 = blend_hex(base00, base06, 0.82), base03 = base00, base04 = "#37474F", base05 = "#90A4AE", base06 = "#525252", base07 = "#08bdba", base08 = "#ff7eb6", base09 = "#ee5396", base10 = "#FF6F00", base11 = "#0f62fe", base12 = "#673AB7", base13 = "#42be65", base14 = "#be95ff", base15 = "#FFAB91", blend = "#FAFAFA", none = "NONE"})
-do end (vim.g)["terminal_color_0"] = oxocarbon.base01
-vim.g["terminal_color_1"] = oxocarbon.base11
-vim.g["terminal_color_2"] = oxocarbon.base14
-vim.g["terminal_color_3"] = oxocarbon.base13
-vim.g["terminal_color_4"] = oxocarbon.base09
-vim.g["terminal_color_5"] = oxocarbon.base15
-vim.g["terminal_color_6"] = oxocarbon.base08
-vim.g["terminal_color_7"] = oxocarbon.base05
-vim.g["terminal_color_8"] = oxocarbon.base03
-vim.g["terminal_color_9"] = oxocarbon.base11
-vim.g["terminal_color_10"] = oxocarbon.base14
-vim.g["terminal_color_11"] = oxocarbon.base13
-vim.g["terminal_color_12"] = oxocarbon.base09
-vim.g["terminal_color_13"] = oxocarbon.base15
-vim.g["terminal_color_14"] = oxocarbon.base07
-vim.g["terminal_color_15"] = oxocarbon.base06
+do end (vim.g)["terminal_color_0"] = oxocarbon.base00
+vim.g["terminal_color_1"] = oxocarbon.base01
+vim.g["terminal_color_2"] = oxocarbon.base02
+vim.g["terminal_color_3"] = oxocarbon.base03
+vim.g["terminal_color_4"] = oxocarbon.base04
+vim.g["terminal_color_5"] = oxocarbon.base05
+vim.g["terminal_color_6"] = oxocarbon.base06
+vim.g["terminal_color_7"] = oxocarbon.base07
+vim.g["terminal_color_8"] = oxocarbon.base08
+vim.g["terminal_color_9"] = oxocarbon.base09
+vim.g["terminal_color_10"] = oxocarbon.base10
+vim.g["terminal_color_11"] = oxocarbon.base11
+vim.g["terminal_color_12"] = oxocarbon.base12
+vim.g["terminal_color_13"] = oxocarbon.base13
+vim.g["terminal_color_14"] = oxocarbon.base14
+vim.g["terminal_color_15"] = oxocarbon.base15
 vim.api.nvim_set_hl(0, "ColorColumn", {fg = oxocarbon.none, bg = oxocarbon.base01})
 vim.api.nvim_set_hl(0, "Cursor", {fg = oxocarbon.base00, bg = oxocarbon.base04})
 vim.api.nvim_set_hl(0, "CursorLine", {fg = oxocarbon.none, bg = oxocarbon.base01})
@@ -345,15 +345,15 @@ vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", {fg = oxocarbon.base15, bg = 
 vim.api.nvim_set_hl(0, "NvimTreeNormal", {fg = oxocarbon.base04, bg = oxocarbon.base00})
 
 
-vim.api.nvim_set_hl(0, "NeoTreeExpander", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeIndentMarker", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeRootName", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeSymbolicLinkTarget", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeModified", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", {fg = oxocarbon.base12, bg = oxocarbon.none})
-vim.api.nvim_set_hl(0, "NeoTreeNormalNC", {fg = oxocarbon.base04, bg = oxocarbon.base00})
-vim.api.nvim_set_hl(0, "NeoTreeNormal", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeExpander", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeIndentMarker", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeRootName", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeSymbolicLinkTarget", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeModified", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", {fg = oxocarbon.base12, bg = oxocarbon.none})
+-- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", {fg = oxocarbon.base04, bg = oxocarbon.base00})
+-- vim.api.nvim_set_hl(0, "NeoTreeNormal", {fg = oxocarbon.base04, bg = oxocarbon.base00})
 
 vim.api.nvim_set_hl(0, "NeogitBranch", {fg = oxocarbon.base10, bg = oxocarbon.none})
 vim.api.nvim_set_hl(0, "NeogitRemote", {fg = oxocarbon.base09, bg = oxocarbon.none})

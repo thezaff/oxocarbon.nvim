66458cc66a07b2914c9a37c3d3fc98f546a40c74
Zafar Khodjaev <zaffenergizante@gmail.com>
2024-07-16 03:30:59 +0500

    transparent

--- lua/oxocarbon/init.lua
+++ lua/oxocarbon/init.lua
@@ -447,4 +447,6 @@ vim.api.nvim_set_hl(0, "VimwikiHeaderChar", { link = "markdownH1" })
 vim.api.nvim_set_hl(0, "VimwikiList", { link = "markdownListMarker" })
 vim.api.nvim_set_hl(0, "VimwikiLink", { link = "markdownUrl" })
 vim.api.nvim_set_hl(0, "VimwikiCode", { link = "markdownCode" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 return { oxocarbon = oxocarbon }

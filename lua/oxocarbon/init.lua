415ef02e89ed86a95e8d4bba936b1318a7e21765
Zafar Khodjaev <zaffenergizante@gmail.com>
2024-07-16 00:21:02 +0500

    bufferline

--- lua/oxocarbon/init.lua
+++ lua/oxocarbon/init.lua
@@ -496,6 +496,9 @@ vim.api.nvim_set_hl(0, "CodeBlock", { fg = oxocarbon.none, bg = oxocarbon.base01
 vim.api.nvim_set_hl(0, "BufferLineDiagnostic", { fg = oxocarbon.base10, bg = oxocarbon.none, bold = true })
 vim.api.nvim_set_hl(0, "BufferLineFill", { bg = oxocarbon.base00 })
 vim.api.nvim_set_hl(0, "BufferLineBufferSelected", { bg = oxocarbon.base01 })
+vim.api.nvim_set_hl(0, "BufferLineMiniIconsAzureSelected", { bg = oxocarbon.base01 })
+vim.api.nvim_set_hl(0, "BufferLineMiniIconsAzureInactive", { bg = oxocarbon.base00 })
+
 vim.api.nvim_set_hl(
 	0,
 	"BufferLineDiagnosticVisible",
